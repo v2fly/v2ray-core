b41d9780cb99fb22dc0d28037ffd77279cd44107
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2020-08-25 22:05:34 +0800

    VProtogen: run without setting GOBIN or GOPATH explicitly

--- infra/vprotogen/main.go
+++ infra/vprotogen/main.go
@@ -8,25 +8,31 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+
+	"v2ray.com/core/common"
 )
 
-var protocMap = map[string]string{
-	"windows": filepath.Join(os.Getenv("GOPATH"), "src", "v2ray.com", "core", ".dev", "protoc", "windows", "protoc.exe"),
-	"darwin":  filepath.Join(os.Getenv("GOPATH"), "src", "v2ray.com", "core", ".dev", "protoc", "macos", "protoc"),
-	"linux":   filepath.Join(os.Getenv("GOPATH"), "src", "v2ray.com", "core", ".dev", "protoc", "linux", "protoc"),
-}
-
 var (
-	repo = flag.String("repo", "", "Repo for protobuf generation, such as v2ray.com/core")
+	repo                           = flag.String("repo", "", "Repo for protobuf generation, such as v2ray.com/core")
+	protoFilesUsingProtocGenGoFast = map[string]bool{"proxy/vless/encoding/addons.proto": true}
 )
 
 func main() {
 	flag.Parse()
 
+	GOPATH := common.GetGOPATH()
+	GOBIN := common.GetGOBIN()
+
+	var protocMap = map[string]string{
+		"windows": filepath.Join(GOPATH, "src", "v2ray.com", "core", ".dev", "protoc", "windows", "protoc.exe"),
+		"darwin":  filepath.Join(GOPATH, "src", "v2ray.com", "core", ".dev", "protoc", "macos", "protoc"),
+		"linux":   filepath.Join(GOPATH, "src", "v2ray.com", "core", ".dev", "protoc", "linux", "protoc"),
+	}
+
 	protofiles := make(map[string][]string)
 	protoc := protocMap[runtime.GOOS]
-	gosrc := filepath.Join(os.Getenv("GOPATH"), "src")
-	reporoot := filepath.Join(os.Getenv("GOPATH"), "src", *repo)
+	gosrc := filepath.Join(GOPATH, "src")
+	reporoot := filepath.Join(GOPATH, "src", *repo)
 
 	filepath.Walk(reporoot, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -47,20 +53,19 @@ func main() {
 		return nil
 	})
 
-	var protoFilesUsingProtocGenGoFast = map[string]bool{"proxy/vless/encoding/addons.proto": true}
-
 	for _, files := range protofiles {
 		for _, absPath := range files {
 			relPath, _ := filepath.Rel(reporoot, absPath)
 			args := make([]string, 0)
 			if protoFilesUsingProtocGenGoFast[relPath] {
-				args = []string{"--proto_path", reporoot, "--gofast_out", gosrc}
+				args = []string{"--proto_path", reporoot, "--gofast_out", gosrc, "--plugin", "protoc-gen-gofast=" + GOBIN + "/protoc-gen-gofast"}
 			} else {
-				args = []string{"--proto_path", reporoot, "--go_out", gosrc, "--go-grpc_out", gosrc}
+				args = []string{"--proto_path", reporoot, "--go_out", gosrc, "--go-grpc_out", gosrc, "--plugin", "protoc-gen-go=" + GOBIN + "/protoc-gen-go", "--plugin", "protoc-gen-go-grpc=" + GOBIN + "/protoc-gen-go-grpc"}
 			}
 			args = append(args, absPath)
 			cmd := exec.Command(protoc, args...)
 			cmd.Env = append(cmd.Env, os.Environ()...)
+			cmd.Env = append(cmd.Env, "GOBIN="+GOBIN)
 			output, err := cmd.CombinedOutput()
 			if len(output) > 0 {
 				fmt.Println(string(output))
--- proto.go
+++ proto.go
@@ -1,8 +1,6 @@
 package core
 
-//go:generate go install "google.golang.org/protobuf/proto"
-//go:generate go install "google.golang.org/protobuf/cmd/protoc-gen-go"
-//go:generate go install "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-//go:generate go install "github.com/gogo/protobuf/protoc-gen-gofast"
-//go:generate go install "v2ray.com/core/infra/vprotogen"
-//go:generate vprotogen -repo v2ray.com/core
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
+//go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+//go:generate go install github.com/gogo/protobuf/protoc-gen-gofast
+//go:generate go run v2ray.com/core/infra/vprotogen -repo v2ray.com/core
