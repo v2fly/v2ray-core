05dbca34cd52d0ae3d88b2f242d0191ae6e98a5e
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2020-09-19 09:51:15 +0800

    Add gzip support for geofiles

--- common/platform/filesystem/file.go
+++ common/platform/filesystem/file.go
@@ -1,19 +1,24 @@
 package filesystem
 
 import (
+	"compress/gzip"
 	"io"
+	"io/ioutil"
 	"os"
 
 	"v2ray.com/core/common/buf"
 	"v2ray.com/core/common/platform"
 )
 
+// FileReaderFunc defines a function type of FileReader
 type FileReaderFunc func(path string) (io.ReadCloser, error)
 
+// NewFileReader returns a FileReader
 var NewFileReader FileReaderFunc = func(path string) (io.ReadCloser, error) {
 	return os.Open(path)
 }
 
+// ReadFile reads a file from path
 func ReadFile(path string) ([]byte, error) {
 	reader, err := NewFileReader(path)
 	if err != nil {
@@ -24,10 +29,33 @@ func ReadFile(path string) ([]byte, error) {
 	return buf.ReadAllToBytes(reader)
 }
 
-func ReadAsset(file string) ([]byte, error) {
-	return ReadFile(platform.GetAssetLocation(file))
+// ReadGzipFile reads a gzip file from path
+func ReadGzipFile(path string) ([]byte, error) {
+	reader, err := NewFileReader(path)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	gzipReader, err := gzip.NewReader(reader)
+	if err != nil {
+		return nil, err
+	}
+	defer gzipReader.Close()
+
+	gunzipBytes, err := ioutil.ReadAll(gzipReader)
+	if err != nil {
+		return nil, err
+	}
+	return gunzipBytes, nil
 }
 
+// ReadAsset reads geofiles
+func ReadAsset(file string) ([]byte, error) {
+	return ReadGzipFile(platform.GetAssetLocation(file))
+}
+
+// CopyFile copies a file from src to dst
 func CopyFile(dst string, src string) error {
 	bytes, err := ReadFile(src)
 	if err != nil {
