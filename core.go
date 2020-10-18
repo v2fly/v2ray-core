834c80836e5cc0c2b22602a4214130d05e3f1707
RPRX <63339210+rprx@users.noreply.github.com>
2020-10-18 06:38:59 +0000

    v4.31.2

--- core.go
+++ core.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	version  = "4.31.1"
+	version  = "4.31.2"
 	build    = "Custom"
 	codename = "V2Fly, a community-driven edition of V2Ray."
 	intro    = "A unified platform for anti-censorship."
