3d378fc280b225745a0a7f3141dc4594043d1562
RPRX <63339210+rprx@users.noreply.github.com>
2020-10-19 14:39:51 +0000

    Fix generate .pb.go under Windows

--- infra/vprotogen/main.go
+++ infra/vprotogen/main.go
@@ -22,6 +22,11 @@ func main() {
 	GOBIN := common.GetGOBIN()
 	protoc := core.ProtocMap[runtime.GOOS]
 
+	EXE := ""
+	if runtime.GOOS == "windows" {
+		EXE = ".exe"
+	}
+
 	protoFilesMap := make(map[string][]string)
 	walkErr := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -50,9 +55,9 @@ func main() {
 		for _, relProtoFile := range files {
 			var args []string
 			if core.ProtoFilesUsingProtocGenGoFast[relProtoFile] {
-				args = []string{"--gofast_out", pwd, "--plugin", "protoc-gen-gofast=" + GOBIN + "/protoc-gen-gofast"}
+				args = []string{"--gofast_out", pwd, "--plugin", "protoc-gen-gofast=" + GOBIN + "/protoc-gen-gofast" + EXE}
 			} else {
-				args = []string{"--go_out", pwd, "--go-grpc_out", pwd, "--plugin", "protoc-gen-go=" + GOBIN + "/protoc-gen-go", "--plugin", "protoc-gen-go-grpc=" + GOBIN + "/protoc-gen-go-grpc"}
+				args = []string{"--go_out", pwd, "--go-grpc_out", pwd, "--plugin", "protoc-gen-go=" + GOBIN + "/protoc-gen-go" + EXE, "--plugin", "protoc-gen-go-grpc=" + GOBIN + "/protoc-gen-go-grpc" + EXE}
 			}
 			args = append(args, relProtoFile)
 			cmd := exec.Command(protoc, args...)
