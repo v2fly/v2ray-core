255bf1e359564604281d413d9cdd0a8e61078bd5
Eken Chan <maskedeken@yahoo.com>
2020-10-22 10:24:12 +0800

    Trojan Protocol Handler implements UserManager

--- proxy/trojan/validator.go
+++ proxy/trojan/validator.go
@@ -1,6 +1,7 @@
 package trojan
 
 import (
+	"strings"
 	"sync"
 
 	"v2ray.com/core/common/protocol"
@@ -8,13 +9,39 @@ import (
 
 // Validator stores valid trojan users
 type Validator struct {
+	// Considering email's usage here, map + sync.Mutex/RWMutex may have better performance.
+	email sync.Map
 	users sync.Map
 }
 
 // Add a trojan user
 func (v *Validator) Add(u *protocol.MemoryUser) error {
-	user := u.Account.(*MemoryAccount)
-	v.users.Store(hexString(user.Key), u)
+	if u.Email != "" {
+		_, loaded := v.email.LoadOrStore(strings.ToLower(u.Email), u)
+		if loaded {
+			return newError("User ", u.Email, " already exists.")
+		}
+	}
+
+	account := u.Account.(*MemoryAccount)
+	v.users.Store(hexString(account.Key), u)
+	return nil
+}
+
+// Del a trojan user
+func (v *Validator) Del(e string) error {
+	if e == "" {
+		return newError("Email must not be empty.")
+	}
+	le := strings.ToLower(e)
+	u, _ := v.email.Load(le)
+	if u == nil {
+		return newError("User ", e, " not found.")
+	}
+	v.email.Delete(le)
+
+	account := u.(*protocol.MemoryUser).Account.(*MemoryAccount)
+	v.users.Delete(hexString(account.Key))
 	return nil
 }
 
