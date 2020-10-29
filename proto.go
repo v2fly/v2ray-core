c4e47e4fbe4b5435788a43500a3ae84e8fdec6aa
JimhHan <50871214+JimhHan@users.noreply.github.com>
2020-10-29 09:04:12 +0800

    fix: vprotogen in windows (#365)

--- proto.go
+++ proto.go
@@ -1,5 +1,7 @@
 package core
 
+import "path/filepath"
+
 //go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go
 //go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
 //go:generate go install -v github.com/gogo/protobuf/protoc-gen-gofast
@@ -7,4 +9,4 @@ package core
 
 // ProtoFilesUsingProtocGenGoFast is the map of Proto files
 // that use `protoc-gen-gofast` to generate pb.go files
-var ProtoFilesUsingProtocGenGoFast = map[string]bool{"proxy/vless/encoding/addons.proto": true}
+var ProtoFilesUsingProtocGenGoFast = map[string]bool{filepath.Join("proxy", "vless", "encoding", "addons.proto"): true}
