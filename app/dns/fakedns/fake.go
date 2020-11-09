ad0e6022d45122a30186fa7a1da4201f1d95ce34
Shelikhoo <xiaokangwang@outlook.com>
2020-11-09 21:20:39 +0000

    Encapsulate Fake DNS as a Instance wide service

--- app/dns/fakedns/fake.go
+++ app/dns/fakedns/fake.go
@@ -15,18 +15,50 @@ type FakeDnsHolder struct {
 	nextIP     *big.Int
 
 	ipRange *gonet.IPNet
+
+	config *FakeDnsPool
+}
+
+func (fkdns *FakeDnsHolder) Type() interface{} {
+	return (*FakeDnsHolder)(nil)
+}
+
+func (fkdns *FakeDnsHolder) Start() error {
+	return fkdns.initializeFromConfig()
+}
+
+func (fkdns *FakeDnsHolder) Close() error {
+	fkdns.domainToIP = nil
+	fkdns.nextIP = nil
+	fkdns.ipRange = nil
+	return nil
 }
 
 var fakeDnsHolder, _ = NewFakeDnsHolder()
 
 func NewFakeDnsHolder() (*FakeDnsHolder, error) {
+	if fkdnsw, err := NewFakeDnsHolderConfigOnly(nil); err != nil {
+		return nil, newError("Unable to create Fake Dns Engine").Base(err).AtError()
+	} else {
+		fkdnsw.initialize("240.0.0.0/8", 65535)
+		return fkdnsw, nil
+	}
+}
+
+func NewFakeDnsHolderConfigOnly(conf *FakeDnsPool) (*FakeDnsHolder, error) {
+	return &FakeDnsHolder{nil, nil, nil, conf}, nil
+}
+
+func (fkdns *FakeDnsHolder) initializeFromConfig() error {
+	return fkdns.initialize(fkdns.config.IpPool, int(fkdns.config.LruSize))
+}
+
+func (fkdns *FakeDnsHolder) initialize(ipPoolCidr string, lruSize int) error {
 	var ipRange *gonet.IPNet
 	var currentIP *big.Int
 
-	var lruSize = 65535
-
-	if ipaddr, ipRangeResult, err := gonet.ParseCIDR("240.0.0.0/8"); err != nil {
-		return nil, newError("Unable to parse CIDR for Fake DNS IP assignment").Base(err).AtError()
+	if ipaddr, ipRangeResult, err := gonet.ParseCIDR(ipPoolCidr); err != nil {
+		return newError("Unable to parse CIDR for Fake DNS IP assignment").Base(err).AtError()
 	} else {
 		ipRange = ipRangeResult
 		currentIP = big.NewInt(0).SetBytes(ipaddr)
@@ -38,10 +70,12 @@ func NewFakeDnsHolder() (*FakeDnsHolder, error) {
 	ones, bits := ipRange.Mask.Size()
 	rooms := bits - ones
 	if lruSize >= 1<<rooms {
-		return nil, newError("LRU size is bigger than subnet size").AtError()
+		return newError("LRU size is bigger than subnet size").AtError()
 	}
-
-	return &FakeDnsHolder{cache.NewLru(lruSize), currentIP, ipRange}, nil
+	fkdns.domainToIP = cache.NewLru(lruSize)
+	fkdns.ipRange = ipRange
+	fkdns.nextIP = currentIP
+	return nil
 }
 
 // GetFakeIPForDomain check and generate a fake IP for a domain name
--- features/dns/fakedns.go
+++ features/dns/fakedns.go
@@ -1,8 +1,12 @@
 package dns
 
-import "v2ray.com/core/common/net"
+import (
+	"v2ray.com/core/common/net"
+	"v2ray.com/core/features"
+)
 
 type FakeDnsEngine interface {
+	features.Feature
 	GetFakeIPForDomain(domain string) []net.Address
 	GetDomainFromFakeDNS(ip net.Address) string
 }
