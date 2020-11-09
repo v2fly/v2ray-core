419dc4d8086f0b0d30ac28ac0c362a51dd552dc2
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2020-11-08 21:58:48 -0500

    fix an edge case when encounter a fake IP in use

--- app/dns/fake.go
+++ app/dns/fake.go
@@ -14,13 +14,20 @@ func GetFakeIPForDomain(domain string) []net.Address {
 	if v, ok := domainToIP.Get(domain); ok {
 		return []net.Address{v.(net.Address)}
 	}
-	as := "240."
-	as += strconv.Itoa((0xff0000&nextIP)>>16) + "."
-	as += strconv.Itoa((0xff00&nextIP)>>8) + "."
-	as += strconv.Itoa(0xff & nextIP)
-	ip := net.ParseAddress(as)
+	var ip net.Address
+	for {
+		as := "240."
+		as += strconv.Itoa((0xff0000&nextIP)>>16) + "."
+		as += strconv.Itoa((0xff00&nextIP)>>8) + "."
+		as += strconv.Itoa(0xff & nextIP)
+		ip = net.ParseAddress(as)
+		nextIP = 0xffffff & (nextIP + 1)
+		// if we run for a long time, we may go back to beginning and start seeing the IP in use
+		if _, ok := domainToIP.GetKeyFromValue(ip); !ok {
+			break
+		}
+	}
 	domainToIP.Put(domain, ip)
-	nextIP = 0xffffff & (nextIP + 1)
 	return []net.Address{ip}
 }
 
@@ -29,8 +36,7 @@ func GetDomainFromFakeDNS(ip net.Address) string {
 	if !ip.Family().IsIP() || ip.String()[:3] != "240" {
 		return ""
 	}
-	k, ok := domainToIP.GetKeyFromValue(ip)
-	if ok {
+	if k, ok := domainToIP.GetKeyFromValue(ip); ok {
 		return k.(string)
 	}
 	return ""
