1ad56a78761eec32e353804f62987a463d66b16d
kslr <kslrwang@gmail.com>
2020-11-16 17:29:35 +0800

    include tcp

---
+++ testing/servers/tcp/port_windows.go
@@ -0,0 +1,32 @@
+package udp
+
+import (
+	"fmt"
+	"v2ray.com/core/common"
+	"v2ray.com/core/common/errors"
+	"v2ray.com/core/common/net"
+)
+
+// PickPort returns an unused UDP port in the system. The port returned is highly likely to be unused, but not guaranteed.
+func PickPort() net.Port {
+	var port uint16
+	ports := []uint16{2479, 4491, 5356, 6044, 6275, 6490, 7002, 28384}
+	for v := range ports {
+		listener, err := net.Listen("tcp4", "127.0.0.1:0")
+		if err != nil {
+			fmt.Println(v, " port is not available")
+			continue
+		} else {
+			addr := conn.LocalAddr().(*net.UDPAddr)
+			port = uint16(addr.Port)
+			_ = conn.Close()
+			break
+		}
+	}
+
+	if port == 0 {
+		common.Must(errors.New("Cannot get a port on windows"))
+	}
+
+	return net.Port(port)
+}
