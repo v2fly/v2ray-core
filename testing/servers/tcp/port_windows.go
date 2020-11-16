a4a2a47d345858e08fda0ad78c0f3189eb441490
kslr <kslrwang@gmail.com>
2020-11-16 18:08:05 +0800

    fix type error

--- testing/servers/tcp/port_windows.go
+++ testing/servers/tcp/port_windows.go
@@ -2,6 +2,7 @@ package tcp
 
 import (
 	"fmt"
+	"strconv"
 	"v2ray.com/core/common"
 	"v2ray.com/core/common/errors"
 	"v2ray.com/core/common/net"
@@ -12,14 +13,14 @@ func PickPort() net.Port {
 	var port uint16
 	ports := []uint16{2479, 4491, 5356, 6044, 6275, 6490, 7002, 28384}
 	for v := range ports {
-		listener, err := net.Listen("tcp4", "127.0.0.1:" + v)
+		listener, err := net.Listen("tcp4", "127.0.0.1:" + strconv.Itoa(v))
 		if err != nil {
 			fmt.Println(v, " port is not available")
 			continue
 		} else {
 			addr := listener.Addr().(*net.TCPAddr)
 			port = uint16(addr.Port)
-			_ = conn.Close()
+			_ = listener.Close()
 			break
 		}
 	}
