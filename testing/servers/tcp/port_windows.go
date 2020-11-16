c07deb9474bd7275268fdc797945e7961be04fa7
kslr <kslrwang@gmail.com>
2020-11-16 17:47:38 +0800

    fix fixed port

--- testing/servers/tcp/port_windows.go
+++ testing/servers/tcp/port_windows.go
@@ -7,12 +7,12 @@ import (
 	"v2ray.com/core/common/net"
 )
 
-// PickPort returns an unused UDP port in the system. The port returned is highly likely to be unused, but not guaranteed.
+// PickPort returns an unused TCP port in the system. The port returned is highly likely to be unused, but not guaranteed.
 func PickPort() net.Port {
 	var port uint16
 	ports := []uint16{2479, 4491, 5356, 6044, 6275, 6490, 7002, 28384}
 	for v := range ports {
-		listener, err := net.Listen("tcp4", "127.0.0.1:0")
+		listener, err := net.Listen("tcp4", "127.0.0.1:" + v)
 		if err != nil {
 			fmt.Println(v, " port is not available")
 			continue
