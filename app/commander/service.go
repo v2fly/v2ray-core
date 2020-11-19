eeb06ef78e36950ad4dab23f7dda603c1d8b5f70
Vigilans <vigilans@foxmail.com>
2020-11-20 00:05:45 +0800

    API: Reflection Service Support

--- app/commander/service.go
+++ app/commander/service.go
@@ -3,11 +3,27 @@
 package commander
 
 import (
+	"context"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"v2ray.com/core/common"
 )
 
 // Service is a Commander service.
 type Service interface {
 	// Register registers the service itself to a gRPC server.
 	Register(*grpc.Server)
+}
+
+type reflectionService struct{}
+
+func (r reflectionService) Register(s *grpc.Server) {
+	reflection.Register(s)
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*ReflectionConfig)(nil), func(ctx context.Context, cfg interface{}) (interface{}, error) {
+		return reflectionService{}, nil
+	}))
 }
