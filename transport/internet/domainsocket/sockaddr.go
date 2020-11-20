7ee503b281c9ab20ea448816f92b005cb853da3e
Chinsyo <chinsyo@sina.cn>
2020-11-20 18:57:48 +0800

    fix lint issue

--- transport/internet/domainsocket/sockaddr.go
+++ transport/internet/domainsocket/sockaddr.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// UnixSockaddrCap represents maximum length limit of a path of unix domain socket.
 func UnixSockaddrCap() int {
 	addr := &unix.RawSockaddrUnix{}
 	path := addr.Path
