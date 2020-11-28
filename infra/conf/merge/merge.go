05ba89ca54b3d3fef1743455b6e97424519715ba
jebbs <qjebbs@gmail.com>
2020-11-28 10:57:29 +0800

    fix a typo

--- infra/conf/merge/merge.go
+++ infra/conf/merge/merge.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package merge provides the capbility to merge mutliple
+Package merge provides the capbility to merge multiple
 JSON files or contents into one output.
 
 Merge Rules:
