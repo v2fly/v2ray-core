c0c500b1c988f6092a2f78a0e5fa16d4300153ac
kslr <kslrwang@gmail.com>
2021-01-11 18:55:51 +0800

    update test

--- testing/servers/udp/udp.go
+++ testing/servers/udp/udp.go
@@ -2,16 +2,14 @@ package udp
 
 import (
 	"fmt"
-	"golang.org/x/net/context"
-
 	"v2ray.com/core/common/net"
 )
 
 type Server struct {
 	Port         net.Port
 	MsgProcessor func(msg []byte) []byte
+	accepting    bool
 	conn         *net.UDPConn
-	cancel       context.CancelFunc
 }
 
 func (server *Server) Start() (net.Destination, error) {
@@ -26,38 +24,32 @@ func (server *Server) Start() (net.Destination, error) {
 	server.Port = net.Port(conn.LocalAddr().(*net.UDPAddr).Port)
 	fmt.Println("UDP server started on port ", server.Port)
 
-	ctx, cancel := context.WithCancel(context.Background())
 	server.conn = conn
-	server.cancel = cancel
-
-	go server.handleConnection(ctx, conn)
+	go server.handleConnection(conn)
 
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	return net.UDPDestination(net.IPAddress(localAddr.IP), net.Port(localAddr.Port)), nil
 }
 
-func (server *Server) handleConnection(ctx context.Context, conn *net.UDPConn) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			buffer := make([]byte, 2*1024)
-			nBytes, addr, err := conn.ReadFromUDP(buffer)
-			if err != nil {
-				fmt.Printf("Failed to read from UDP: %v\n", err)
-				continue
-			}
-
-			response := server.MsgProcessor(buffer[:nBytes])
-			if _, err := conn.WriteToUDP(response, addr); err != nil {
-				fmt.Println("Failed to write to UDP: ", err.Error())
-			}
+func (server *Server) handleConnection(conn *net.UDPConn) {
+	server.accepting = true
+	for server.accepting {
+		buffer := make([]byte, 2*1024)
+		nBytes, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Printf("Failed to read from UDP: %v\n", err)
+			continue
 		}
+
+		response := server.MsgProcessor(buffer[:nBytes])
+		if _, err := conn.WriteToUDP(response, addr); err != nil {
+			fmt.Println("Failed to write to UDP: ", err.Error())
+		}
+
 	}
 }
 
 func (server *Server) Close() error {
-	server.cancel()
+	server.accepting = false
 	return server.conn.Close()
 }
