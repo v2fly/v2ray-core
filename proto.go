c288c3062ad5032abb455ac382129af9f655b8d9
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2021-02-19 12:23:23 +0800

    Fix

--- proto.go
+++ proto.go
@@ -2,10 +2,10 @@ package core
 
 import "path/filepath"
 
-//go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go
-//go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
-//go:generate go install -v github.com/gogo/protobuf/protoc-gen-gofast
-//go:generate go run ./infra/vprotogen/main.go
+//go:generate go get -v google.golang.org/protobuf/cmd/protoc-gen-go
+//go:generate go get -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
+//go:generate go get -v github.com/gogo/protobuf/protoc-gen-gofast
+//go:generate go run ./infra/vprotogen/
 
 // ProtoFilesUsingProtocGenGoFast is the map of Proto files
 // that use `protoc-gen-gofast` to generate pb.go files
