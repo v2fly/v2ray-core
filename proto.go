0e71086b2a5c3e3ac16a861875f2764a3ad0399f
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2021-02-20 20:31:15 +0800

    Fix: go generate command for Go v1.16

--- proto.go
+++ proto.go
@@ -2,9 +2,9 @@ package core
 
 import "path/filepath"
 
-//go:generate go get -v google.golang.org/protobuf/cmd/protoc-gen-go
-//go:generate go get -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
-//go:generate go get -v github.com/gogo/protobuf/protoc-gen-gofast
+//go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go@latest
+//go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+//go:generate go install -v github.com/gogo/protobuf/protoc-gen-gofast@latest
 //go:generate go run ./infra/vprotogen/
 
 // ProtoFilesUsingProtocGenGoFast is the map of Proto files
