d79b8291c3376c3d65a8f6acbd6aa6ecaedd4fac
Shelikhoo <xiaokangwang@outlook.com>
2021-03-06 16:26:15 +0000

    network observatory service

--- app/observatory/observer.go
+++ app/observatory/observer.go
@@ -2,10 +2,20 @@ package observatory
 
 import (
 	"context"
+	"github.com/golang/protobuf/proto"
+	core "github.com/v2fly/v2ray-core/v4"
+	"github.com/v2fly/v2ray-core/v4/common"
+	v2net "github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/common/signal/done"
+	"github.com/v2fly/v2ray-core/v4/common/task"
 	"github.com/v2fly/v2ray-core/v4/features/extension"
 	"github.com/v2fly/v2ray-core/v4/features/outbound"
+	"github.com/v2fly/v2ray-core/v4/transport/internet/tagged"
+	"net"
+	"net/http"
+	"net/url"
 	"sync"
+	"time"
 )
 
 type Observer struct {
@@ -13,11 +23,15 @@ type Observer struct {
 	ctx    context.Context
 
 	statusLock sync.Mutex
-	status     []OutboundStatus
+	status     []*OutboundStatus
 
 	finished *done.Instance
 
 	ohm outbound.Manager
+}
+
+func (o *Observer) GetObservation(ctx context.Context) (proto.Message, error) {
+	return &ObservationResult{Status: o.status}, nil
 }
 
 func (o *Observer) Type() interface{} {
@@ -41,13 +55,136 @@ func (o *Observer) background() {
 			newError("outbound.Manager is not a HandlerSelector").WriteToLog()
 			return
 		}
+
 		outbounds := hs.Select(o.config.SubjectSelector)
+
+		o.updateStatus(outbounds)
+
+		for _, v := range outbounds {
+			result := o.probe(v)
+			o.updateStatusForResult(v, &result)
+			if o.finished.Done() {
+				return
+			}
+			time.Sleep(time.Second * 10)
+		}
 
 	}
 }
+
 func (o *Observer) updateStatus(outbounds []string) {
 	o.statusLock.Lock()
 	defer o.statusLock.Unlock()
+	//TODO should remove old inbound that is removed
+}
 
-	o.status
+func (o *Observer) probe(outbound string) ProbeResult {
+	httpTransport := http.Transport{
+		Proxy: func(*http.Request) (*url.URL, error) {
+			return nil, nil
+		},
+		DialContext: func(ctx context.Context, network string, addr string) (net.Conn, error) {
+			var connection net.Conn
+			taskErr := task.Run(ctx, func() error {
+				//MUST use V2Fly's built in context system
+				dest, err := v2net.ParseDestination(network + ":" + addr)
+				if err != nil {
+					return newError("cannot understand address").Base(err)
+				}
+				conn, err := tagged.Dialer(o.ctx, dest, outbound)
+				if err != nil {
+					return newError("cannot dial remote address", dest).Base(err)
+				}
+				connection = conn
+				return nil
+			})
+			if taskErr != nil {
+				return nil, newError("cannot finish connection").Base(taskErr)
+			}
+			return connection, nil
+		},
+		TLSHandshakeTimeout: time.Duration(time.Second * 5),
+	}
+	httpClient := &http.Client{
+		Transport: &httpTransport,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Jar:     nil,
+		Timeout: time.Duration(time.Second * 5),
+	}
+	var GETTime time.Duration
+	err := task.Run(o.ctx, func() error {
+		startTime := time.Now()
+		response, err := httpClient.Get("https://api.v2fly.org/checkConnection.svgz")
+		if err != nil {
+			return newError("outbound failed to relay connection").Base(err)
+		}
+		if response.Body != nil {
+			response.Body.Close()
+		}
+		endTime := time.Now()
+		GETTime = endTime.Sub(startTime)
+		return nil
+	})
+	if err != nil {
+		newError("the outbound ", outbound, "is dead:").Base(err).AtInfo().WriteToLog()
+		return ProbeResult{Alive: false, LastErrorReason: err.Error()}
+	}
+	newError("the outbound ", outbound, "is alive:", GETTime.Seconds()).AtInfo().WriteToLog()
+	return ProbeResult{Alive: true, Delay: GETTime.Milliseconds()}
+}
+
+func (o *Observer) updateStatusForResult(outbound string, result *ProbeResult) {
+	o.statusLock.Lock()
+	defer o.statusLock.Unlock()
+	var status *OutboundStatus
+	if location := o.findStatusLocationLockHolderOnly(outbound); location != -1 {
+		status = o.status[location]
+	} else {
+		status = &OutboundStatus{}
+		o.status = append(o.status, status)
+	}
+
+	status.LastTryTime = time.Now().Unix()
+	status.OutboundTag = outbound
+	status.Alive = result.Alive
+	if result.Alive {
+		status.Delay = result.Delay
+		status.LastSeenTime = status.LastTryTime
+		status.LastErrorReason = ""
+	} else {
+		status.LastErrorReason = result.LastErrorReason
+		status.Delay = 99999999
+	}
+}
+
+func (o *Observer) findStatusLocationLockHolderOnly(outbound string) int {
+	for i, v := range o.status {
+		if v.OutboundTag == outbound {
+			return i
+		}
+	}
+	return -1
+}
+
+func New(ctx context.Context, config *Config) (*Observer, error) {
+	var outboundManager outbound.Manager
+	err := core.RequireFeatures(ctx, func(om outbound.Manager) {
+		outboundManager = om
+	})
+	if err != nil {
+		return nil, newError("Cannot get depended features").Base(err)
+	}
+	return &Observer{
+		config: config,
+		ctx:    ctx,
+		ohm:    outboundManager,
+	}, nil
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return New(ctx, config.(*Config))
+	}))
 }
