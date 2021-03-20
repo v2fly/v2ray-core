aac35a833d9c7ca75f719b7731c7e7de2eaa98ba
Shelikhoo <xiaokangwang@outlook.com>
2021-03-20 21:40:38 +0000

    added hash of browser forwarding asset to embedded hash

--- common/platform/securedload/embedded.go
+++ common/platform/securedload/embedded.go
@@ -3,4 +3,7 @@ package securedload
 const allowedHashes = `SHA256 (!#project==v2fly) = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 SHA256 (!#version==embedded) = ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 SHA256 (subscriptions/subscriptionsDefinition.v2flyTemplate) = 3f165dba7de0d7c506fbdff3275ea64b76f307df435316a3ea0914ee957793ab
+SHA256 (browserforwarder/index.html) = 34f2c573724256421ade769bda18eeac85172bf0aaed00d7b90e41e843a2caef
+SHA256 (browserforwarder/index.js) = 7de6de0bb7a1c8838f20fcba6bd9b02e1199d116b053836ad622184588d9a37e
+
 `
