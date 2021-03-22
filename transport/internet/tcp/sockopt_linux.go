fca24899130db56e2a787ebf087511a68194f40e
mzz2017 <mzz@tuta.io>
2021-03-22 21:38:24 +0800

    fix(tproxy): the problem that cannot find ipv6 destination in redirect mode of tproxy

--- transport/internet/tcp/sockopt_linux.go
+++ transport/internet/tcp/sockopt_linux.go
@@ -4,13 +4,14 @@
 package tcp
 
 import (
-	"syscall"
-
 	"github.com/v2fly/v2ray-core/v4/common/net"
 	"github.com/v2fly/v2ray-core/v4/transport/internet"
+	"syscall"
 )
 
-const SO_ORIGINAL_DST = 80 // nolint: golint,stylecheck
+const (
+	SO_ORIGINAL_DST = 80 // nolint: golint,stylecheck
+)
 
 func GetOriginalDestination(conn internet.Connection) (net.Destination, error) {
 	sysrawconn, f := conn.(syscall.Conn)
@@ -23,14 +24,37 @@ func GetOriginalDestination(conn internet.Connection) (net.Destination, error) {
 	}
 	var dest net.Destination
 	err = rawConn.Control(func(fd uintptr) {
-		addr, err := syscall.GetsockoptIPv6Mreq(int(fd), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
-		if err != nil {
-			newError("failed to call getsockopt").Base(err).WriteToLog()
+		var remoteIP net.IP
+		switch addr := conn.RemoteAddr().(type) {
+		case *net.TCPAddr:
+			remoteIP = addr.IP
+		case *net.UDPAddr:
+			remoteIP = addr.IP
+		default:
+			newError("failed to call getsockopt").WriteToLog()
 			return
 		}
-		ip := net.IPAddress(addr.Multiaddr[4:8])
-		port := uint16(addr.Multiaddr[2])<<8 + uint16(addr.Multiaddr[3])
-		dest = net.TCPDestination(ip, net.Port(port))
+		if remoteIP.To4() != nil {
+			// ipv4
+			addr, err := syscall.GetsockoptIPv6Mreq(int(fd), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
+			if err != nil {
+				newError("failed to call getsockopt").Base(err).WriteToLog()
+				return
+			}
+			ip := net.IPAddress(addr.Multiaddr[4:8])
+			port := uint16(addr.Multiaddr[2])<<8 + uint16(addr.Multiaddr[3])
+			dest = net.TCPDestination(ip, net.Port(port))
+		} else {
+			// ipv6
+			addr, err := syscall.GetsockoptIPv6MTUInfo(int(fd), syscall.IPPROTO_IPV6, SO_ORIGINAL_DST)
+			if err != nil {
+				newError("failed to call getsockopt").Base(err).WriteToLog()
+				return
+			}
+			ip := net.IPAddress(addr.Addr.Addr[:])
+			port := net.PortFromBytes([]byte{byte(addr.Addr.Port), byte(addr.Addr.Port >> 8)})
+			dest = net.TCPDestination(ip, port)
+		}
 	})
 	if err != nil {
 		return net.Destination{}, newError("failed to control connection").Base(err)
