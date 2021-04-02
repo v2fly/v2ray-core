e31bfe02f0490f65a4c1a39a4b45ce56349a8033
Shelikhoo <xiaokangwang@outlook.com>
2021-04-02 12:12:27 +0100

    Revert "feat: export (const.)core.v2rayKey (#831)"
    This reverts commit 102e3295

--- context.go
+++ context.go
@@ -9,12 +9,11 @@ import (
 // V2rayKey is the key type of Instance in Context, exported for test.
 type V2rayKey int
 
-// V2rayKeyValue is the key value of Instance in Context, exported for test.
-const V2rayKeyValue V2rayKey = 1
+const v2rayKey V2rayKey = 1
 
 // FromContext returns an Instance from the given context, or nil if the context doesn't contain one.
 func FromContext(ctx context.Context) *Instance {
-	if s, ok := ctx.Value(V2rayKeyValue).(*Instance); ok {
+	if s, ok := ctx.Value(v2rayKey).(*Instance); ok {
 		return s
 	}
 	return nil
