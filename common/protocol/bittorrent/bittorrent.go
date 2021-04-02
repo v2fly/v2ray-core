38b686bdda4191d459e7291d4a9feb9d62b54a1d
rurirei <72071920+rurirei@users.noreply.github.com>
2021-04-02 20:49:55 +0800

    Update bittorrent.go

--- common/protocol/bittorrent/bittorrent.go
+++ common/protocol/bittorrent/bittorrent.go
@@ -19,14 +19,22 @@ func (h *SniffHeader) Domain() string {
 
 var errNotBittorrent = errors.New("not bittorrent header")
 
-func SniffBittorrent(b []byte) (*SniffHeader, error) {
+func SniffProtocolBittorrent(b []byte) (*SniffHeader, error) {
+	h := &SniffHeader{}
+
 	if len(b) < 20 {
 		return nil, common.ErrNoClue
 	}
 
 	if b[0] == 19 && string(b[1:20]) == "BitTorrent protocol" {
-		return &SniffHeader{}, nil
+		return h, nil
 	}
 
 	return nil, errNotBittorrent
+}
+
+func SniffDomainBittorrent(b []byte) (*SniffHeader, error) {
+	h, err := SniffProtocolBittorrent(b)
+
+	return h, err
 }
