b0f30023b849dc1f7c6d5aa31e98a329623e05a9
rurirei <72071920+rurirei@users.noreply.github.com>
2021-04-02 20:36:49 +0800

    domainSniffer on FakeDNSSniffer

--- app/dispatcher/fakednssniffer.go
+++ app/dispatcher/fakednssniffer.go
@@ -13,19 +13,19 @@ import (
 )
 
 // newFakeDNSSniffer Create a Fake DNS metadata sniffer
-func newFakeDNSSniffer(ctx context.Context) (protocolSnifferWithMetadata, error) {
+func newFakeDNSSniffer(ctx context.Context) (snifferWithMetadata, error) {
 	var fakeDNSEngine dns.FakeDNSEngine
 	err := core.RequireFeatures(ctx, func(fdns dns.FakeDNSEngine) {
 		fakeDNSEngine = fdns
 	})
 	if err != nil {
-		return protocolSnifferWithMetadata{}, err
+		return snifferWithMetadata{}, err
 	}
 	if fakeDNSEngine == nil {
 		errNotInit := newError("FakeDNSEngine is not initialized, but such a sniffer is used").AtError()
-		return protocolSnifferWithMetadata{}, errNotInit
+		return snifferWithMetadata{}, errNotInit
 	}
-	return protocolSnifferWithMetadata{protocolSniffer: func(ctx context.Context, bytes []byte) (SniffResult, error) {
+	return snifferWithMetadata{domainSniffer: func(ctx context.Context, bytes []byte) (SniffResult, error) {
 		Target := session.OutboundFromContext(ctx).Target
 		if Target.Network == net.Network_TCP || Target.Network == net.Network_UDP {
 			domainFromFakeDNS := fakeDNSEngine.GetDomainFromFakeDNS(Target.Address)
@@ -34,6 +34,8 @@ func newFakeDNSSniffer(ctx context.Context) (protocolSnifferWithMetadata, error)
 				return &fakeDNSSniffResult{domainName: domainFromFakeDNS}, nil
 			}
 		}
+		return nil, common.ErrNoClue
+	}, protocolSniffer: func(ctx context.Context, bytes []byte) (SniffResult, error) {
 		return nil, common.ErrNoClue
 	}, metadataSniffer: true}, nil
 }
