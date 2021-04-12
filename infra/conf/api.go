726f023a5dddcc4ebb33e777cb1432ab0b0e5f17
Shelikhoo <xiaokangwang@outlook.com>
2021-04-12 01:18:20 +0100

    fix incorrect service name

--- infra/conf/api.go
+++ infra/conf/api.go
@@ -1,7 +1,6 @@
 package conf
 
 import (
-	"github.com/v2fly/v2ray-core/v4/app/observatory"
 	"strings"
 
 	"github.com/jhump/protoreflect/desc"
@@ -9,6 +8,7 @@ import (
 
 	"github.com/v2fly/v2ray-core/v4/app/commander"
 	loggerservice "github.com/v2fly/v2ray-core/v4/app/log/command"
+	observatoryservice "github.com/v2fly/v2ray-core/v4/app/observatory/command"
 	handlerservice "github.com/v2fly/v2ray-core/v4/app/proxyman/command"
 	statsservice "github.com/v2fly/v2ray-core/v4/app/stats/command"
 	"github.com/v2fly/v2ray-core/v4/common/serial"
@@ -36,7 +36,7 @@ func (c *APIConfig) Build() (*commander.Config, error) {
 		case "statsservice":
 			services = append(services, serial.ToTypedMessage(&statsservice.Config{}))
 		case "observatoryservice":
-			services = append(services, serial.ToTypedMessage(&observatory.Config{}))
+			services = append(services, serial.ToTypedMessage(&observatoryservice.Config{}))
 		default:
 			if !strings.HasPrefix(s, "#") {
 				continue
