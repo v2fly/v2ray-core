86a3ee249a0c5d03b7cf869de5663936a343df1a
Xiaokang Wang <xiaokangwang@outlook.com>
2021-04-29 17:29:12 +0100

    Update core.go

--- core.go
+++ core.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	version  = "4.38.1"
+	version  = "4.38.2"
 	build    = "Custom"
 	codename = "V2Fly, a community-driven edition of V2Ray."
 	intro    = "A unified platform for anti-censorship."
