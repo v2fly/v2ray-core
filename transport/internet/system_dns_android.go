3afdfdc625e3f70305858add2df66be03f3a2d36
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-04 23:45:08 +0800

    use init

--- transport/internet/system_dns_android.go
+++ transport/internet/system_dns_android.go
@@ -9,14 +9,14 @@ import (
 
 const SystemDNS = "8.8.8.8:53"
 
-type DNSResolverFunc func() *net.Resolver
-
-var NewDNSResolver DNSResolverFunc = func() *net.Resolver {
-	return &net.Resolver{
-		PreferGo: true,
-		Dial: func(ctx context.Context, network, _ string) (net.Conn, error) {
-			var dialer net.Dialer
-			return dialer.DialContext(ctx, network, SystemDNS)
-		},
+func init() {
+	NewDNSResolver = func() *net.Resolver {
+		return &net.Resolver{
+			PreferGo: true,
+			Dial: func(ctx context.Context, network, _ string) (net.Conn, error) {
+				var dialer net.Dialer
+				return dialer.DialContext(ctx, network, SystemDNS)
+			},
+		}
 	}
 }
