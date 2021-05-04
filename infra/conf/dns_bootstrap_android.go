de31ea36d1b0f8e4c33927341534da321ad4e2e0
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-04 10:59:33 +0800

    add alternative bootstrapDialer

--- infra/conf/dns_bootstrap_android.go
+++ infra/conf/dns_bootstrap_android.go
@@ -7,15 +7,19 @@ import (
 	"net"
 )
 
-const bootstrapDNS = "8.8.8.8:53"
+type DialerFunc func(context.Context, string, string) (net.Conn, error)
+
+var BootstrapDialer DialerFunc = func(ctx context.Context, network, _ string) (net.Conn, error) {
+	var dialer net.Dialer
+	return dialer.DialContext(ctx, network, BootstrapDNS)
+}
+
+var BootstrapDNS = "8.8.8.8:53"
 
 func init() {
 	net.DefaultResolver = &net.Resolver{
 		PreferGo: true,
-		Dial: func(ctx context.Context, network, _ string) (net.Conn, error) {
-			var dialer net.Dialer
-			return dialer.DialContext(ctx, network, bootstrapDNS)
-		},
+		Dial: BootstrapDialer,
 	}
-	newError("Android Bootstrap DNS: ", bootstrapDNS).AtWarning().WriteToLog()
+	newError("Android Bootstrap DNS: ", BootstrapDNS).AtWarning().WriteToLog()
 }
