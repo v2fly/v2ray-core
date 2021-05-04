a802bdb89bace7bfa2c4547e79db07b3ffaa6e89
Shelikhoo <xiaokangwang@outlook.com>
2021-05-04 11:19:03 +0100

    memconservative geofile loader

---
+++ infra/conf/geodata/memconservative/errors.generated.go
@@ -0,0 +1,9 @@
+package memconservative
+
+import "github.com/v2fly/v2ray-core/v4/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}
