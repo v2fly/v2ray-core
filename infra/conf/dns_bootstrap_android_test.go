c7158f40828529527d21f3788be7042927bdf2b1
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-04 11:21:12 +0800

    Update dns_bootstrap_android_test.go

--- infra/conf/dns_bootstrap_android_test.go
+++ infra/conf/dns_bootstrap_android_test.go
@@ -4,12 +4,29 @@ package conf
 
 import (
 	"context"
-	"net"
+	"fmt"
+	gonet "net"
 	"testing"
+
+	"github.com/v2fly/v2ray-core/v4/common/net"
+	"github.com/v2fly/v2ray-core/v4/transport/internet"
 )
 
 func TestBootstrapDNS(t *testing.T) {
-	if ips, err := net.LookupIP("www.google.com"); len(ips) == 0 {
+	if ips, err := gonet.LookupIP("www.google.com"); len(ips) == 0 {
 		t.Errorf("failed to lookupIP with BootstrapDNS, %v", err)
 	}
+}
+
+func TestBootstrapDNSWithV2raySystemDialer(t *testing.T) {
+	BootstrapDialer := func(ctx context.Context, network, _ string) (gonet.Conn, error) {
+		dest, err := net.ParseDestination(fmt.Sprintf("%s:%s", network, BootstrapDNS))
+		if err != nil {
+			return nil, err
+		}
+		return internet.DialSystem(ctx, dest, nil)
+	}
+	UseAlternativeBootstrapDNS(BootstrapDialer)
+
+	TestBootstrapDNS(t *testing.T)
 }
