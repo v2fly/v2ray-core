56614cca69460195b621bed43bdfb4d2fd55aa14
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-04 17:57:03 +0800

    noneed TestSystemDNSResolver

--- transport/internet/system_dns_test.go
+++ transport/internet/system_dns_test.go
@@ -2,34 +2,12 @@ package internet
 
 import (
 	"context"
-	"fmt"
-	gonet "net"
 	"testing"
-
-	"github.com/v2fly/v2ray-core/v4/common/net"
 )
 
 func TestDNSResolver(t *testing.T) {
 	resolver := NewDNSResolver()
 	if ips, err := resolver.LookupIP(context.Background(), "ip", "www.google.com"); err != nil {
-		t.Errorf("failed to lookupIP with BootstrapDNS, %v, %v", ips, err)
+		t.Errorf("failed to lookupIP, %v, %v", ips, err)
 	}
-}
-
-func TestSystemDNSResolver(t *testing.T) {
-	NewDNSResolver = func() *gonet.Resolver {
-		return &gonet.Resolver{
-			PreferGo: true,
-			Dial: func(ctx context.Context, network, _ string) (gonet.Conn, error) {
-				const systemDNS = "8.8.8.8:53"
-				dest, err := net.ParseDestination(fmt.Sprintf("%s:%s", network, systemDNS))
-				if err != nil {
-					return nil, err
-				}
-				return DialSystem(ctx, dest, nil)
-			},
-		}
-	}
-
-	TestDNSResolver(t)
 }
