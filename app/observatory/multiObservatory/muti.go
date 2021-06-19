3401963b570b8338372191aecb909a40d43e5ca5
Shelikhoo <xiaokangwang@outlook.com>
2021-06-19 10:49:34 +0100

    multi-observer

---
+++ app/observatory/multiObservatory/muti.go
@@ -0,0 +1,38 @@
+package multiObservatory
+
+import (
+	"context"
+	"github.com/golang/protobuf/proto"
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/taggedfeatures"
+	"github.com/v2fly/v2ray-core/v4/features"
+	"github.com/v2fly/v2ray-core/v4/features/extension"
+)
+
+type Observer struct {
+	features.TaggedFeatures
+	config *Config
+	ctx    context.Context
+}
+
+func (o Observer) GetObservation(ctx context.Context) (proto.Message, error) {
+	return common.Must2(o.GetFeaturesByTag("")).(extension.Observatory).GetObservation(ctx)
+}
+
+func (o Observer) Type() interface{} {
+	return extension.ObservatoryType()
+}
+
+func New(ctx context.Context, config *Config) (*Observer, error) {
+	holder, err := taggedfeatures.NewHolderFromConfig(ctx, config.Holders, extension.ObservatoryType())
+	if err != nil {
+		return nil, err
+	}
+	return &Observer{config: config, ctx: ctx, TaggedFeatures: holder}, nil
+}
+
+func init() {
+	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return New(ctx, config.(*Config))
+	}))
+}
