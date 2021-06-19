31c4df51e12768d74bd5469d024b8e65a4326d61
Shelikhoo <xiaokangwang@outlook.com>
2021-06-19 10:50:30 +0100

    added Runnable to Tagged Features Interface

--- features/feature.go
+++ features/feature.go
@@ -16,6 +16,8 @@ func PrintDeprecatedFeatureWarning(feature string) {
 	newError("You are using a deprecated feature: " + feature + ". Please update your config file with latest configuration format, or update your client software.").WriteToLog()
 }
 
+// TaggedFeatures unstable
 type TaggedFeatures interface {
 	GetFeaturesByTag(tag string) (Feature, error)
+	common.Runnable
 }
