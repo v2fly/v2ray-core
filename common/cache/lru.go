4811f7cfab400545bd0872c9d18be4d5d5529d7f
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2021-07-15 01:07:59 +0800

    Chore: refine LRU

--- common/cache/lru.go
+++ common/cache/lru.go
@@ -1,9 +1,6 @@
 package cache
 
-import (
-	"container/list"
-	sync "sync"
-)
+import "sync"
 
 // Lru simple, fast lru cache implementation
 type Lru interface {
@@ -13,64 +10,110 @@ type Lru interface {
 }
 
 type lru struct {
-	capacity         int
-	doubleLinkedlist *list.List
-	keyToElement     *sync.Map
-	valueToElement   *sync.Map
-	mu               *sync.Mutex
+	capacity       int
+	count          int
+	head           *lruElement
+	tail           *lruElement
+	keyToElement   *sync.Map
+	valueToElement *sync.Map
+	mu             *sync.Mutex
 }
 
 type lruElement struct {
 	key   interface{}
 	value interface{}
+	prev  *lruElement
+	next  *lruElement
 }
 
 // NewLru init a lru cache
 func NewLru(cap int) Lru {
-	return lru{
-		capacity:         cap,
-		doubleLinkedlist: list.New(),
-		keyToElement:     new(sync.Map),
-		valueToElement:   new(sync.Map),
-		mu:               new(sync.Mutex),
+	return &lru{
+		capacity:       cap,
+		keyToElement:   new(sync.Map),
+		valueToElement: new(sync.Map),
+		mu:             new(sync.Mutex),
 	}
 }
 
-func (l lru) Get(key interface{}) (value interface{}, ok bool) {
+func (l *lru) Get(key interface{}) (value interface{}, ok bool) {
 	if v, ok := l.keyToElement.Load(key); ok {
-		element := v.(*list.Element)
-		l.doubleLinkedlist.MoveBefore(element, l.doubleLinkedlist.Front())
-		return element.Value.(lruElement).value, true
-	}
-	return nil, false
-}
-
-func (l lru) GetKeyFromValue(value interface{}) (key interface{}, ok bool) {
-	if k, ok := l.valueToElement.Load(value); ok {
-		element := k.(*list.Element)
-		l.doubleLinkedlist.MoveBefore(element, l.doubleLinkedlist.Front())
-		return element.Value.(lruElement).key, true
-	}
-	return nil, false
-}
-
-func (l lru) Put(key, value interface{}) {
-	e := lruElement{key, value}
-	if v, ok := l.keyToElement.Load(key); ok {
-		element := v.(*list.Element)
-		element.Value = e
-		l.doubleLinkedlist.MoveBefore(element, l.doubleLinkedlist.Front())
-	} else {
+		element := v.(*lruElement)
 		l.mu.Lock()
-		element := l.doubleLinkedlist.PushFront(e)
+		l.unlink(element)
+		l.link(element)
+		l.mu.Unlock()
+		return element.value, true
+	}
+	return nil, false
+}
+
+func (l *lru) GetKeyFromValue(value interface{}) (key interface{}, ok bool) {
+	if k, ok := l.valueToElement.Load(value); ok {
+		element := k.(*lruElement)
+		l.mu.Lock()
+		l.unlink(element)
+		l.link(element)
+		l.mu.Unlock()
+		return element.key, true
+	}
+	return nil, false
+}
+
+func (l *lru) Put(key, value interface{}) {
+	l.mu.Lock()
+	if v, ok := l.keyToElement.Load(key); ok {
+		element := v.(*lruElement)
+		element.value = value
+		l.unlink(element)
+		l.link(element)
+	} else {
+		element := &lruElement{key: key, value: value}
 		l.keyToElement.Store(key, element)
 		l.valueToElement.Store(value, element)
-		if l.doubleLinkedlist.Len() > l.capacity {
-			toBeRemove := l.doubleLinkedlist.Back()
-			l.doubleLinkedlist.Remove(toBeRemove)
-			l.keyToElement.Delete(toBeRemove.Value.(lruElement).key)
-			l.valueToElement.Delete(toBeRemove.Value.(lruElement).value)
-		}
-		l.mu.Unlock()
+		l.link(element)
+		l.count++
 	}
+	if l.count > l.capacity {
+		l.count--
+		l.keyToElement.Delete(l.tail.key)
+		l.valueToElement.Delete(l.tail.value)
+		l.unlink(l.tail)
+	}
+	l.mu.Unlock()
+}
+
+func (l *lru) link(element *lruElement) {
+	element.prev = nil
+	element.next = l.head
+	if l.head != nil {
+		l.head.prev = element
+	}
+	l.head = element
+
+	if l.tail == nil {
+		l.tail = element
+		l.tail.next = nil
+	}
+}
+
+func (l *lru) unlink(element *lruElement) {
+	if element == l.head {
+		l.head = element.next
+		if element.next != nil {
+			element.next.prev = nil
+		}
+		element.next = nil
+		return
+	}
+	if element == l.tail {
+		l.tail = element.prev
+		if element.prev != nil {
+			element.prev.next = nil
+		}
+		element.prev = nil
+		return
+	}
+	element.prev.next = element.next
+	element.next.prev = element.prev
 }
