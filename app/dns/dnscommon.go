e5bdf0da2cb9127362ddd8f7039cb7cf6a5577c7
rurirei <72071920+rurirei@users.noreply.github.com>
2021-08-04 08:11:14 +0800

    typo

--- app/dns/dnscommon.go
+++ app/dns/dnscommon.go
@@ -213,7 +213,7 @@ L:
 		case dnsmessage.TypeAAAA:
 			ans, err := parser.AAAAResource()
 			if err != nil {
-				newError("failed to parse A record for domain: ", ah.Name).Base(err).WriteToLog()
+				newError("failed to parse AAAA record for domain: ", ah.Name).Base(err).WriteToLog()
 				break L
 			}
 			ipRecord.IP = append(ipRecord.IP, net.IPAddress(ans.AAAA[:]))
