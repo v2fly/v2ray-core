6b15c7f04fe3a26bc4a54a1051c85cd5dff65dfa
Shelikhoo <xiaokangwang@outlook.com>
2021-09-04 21:16:01 +0100

    added socks simplified config

---
+++ proxy/socks/simplified/config.go
@@ -0,0 +1,33 @@
+package simplified
+
+import (
+	"context"
+	"github.com/v2fly/v2ray-core/v4/common"
+	"github.com/v2fly/v2ray-core/v4/common/protocol"
+	"github.com/v2fly/v2ray-core/v4/proxy/socks"
+)
+
+func init() {
+	common.Must(common.RegisterConfig((*ServerConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		simplifiedServer := config.(*ServerConfig)
+		fullServer := &socks.ServerConfig{
+			AuthType:   socks.AuthType_NO_AUTH,
+			Address:    simplifiedServer.Address,
+			UdpEnabled: simplifiedServer.UdpEnabled,
+		}
+		return socks.NewServer(ctx, fullServer)
+	}))
+
+	common.Must(common.RegisterConfig((*ClientConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		simplifiedClient := config.(*ClientConfig)
+		fullClient := &socks.ClientConfig{
+			Server: []*protocol.ServerEndpoint{
+				{
+					Address: simplifiedClient.Address,
+					Port:    simplifiedClient.Port,
+				},
+			},
+		}
+		return socks.NewClient(ctx, fullClient)
+	}))
+}
