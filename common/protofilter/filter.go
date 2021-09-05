179d6568fcfe381df289facc4a85a193acd04909
Shelikhoo <xiaokangwang@outlook.com>
2021-09-05 16:29:12 +0100

    fix auto file content loader

--- common/protofilter/filter.go
+++ common/protofilter/filter.go
@@ -26,7 +26,7 @@ func filterMessage(ctx context.Context, message protoreflect.Message) error {
 	var fileReadingQueue []fileRead
 	message.Range(func(descriptor protoreflect.FieldDescriptor, value protoreflect.Value) bool {
 		v2extension, ferr := protoext.GetFieldOptions(descriptor)
-		if ferr != nil {
+		if ferr == nil {
 			if v2extension.Forbidden {
 				if value.Bool() {
 					err = newError("a forbidden value is set ", descriptor.FullName())
