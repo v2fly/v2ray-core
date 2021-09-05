08b841aee147b5249da4a3c8a40196dc1795cb80
Shelikhoo <xiaokangwang@outlook.com>
2021-09-05 15:02:31 +0100

    added filesystem default implementation

---
+++ common/environment/envimpl/fs.go
@@ -0,0 +1,26 @@
+package envimpl
+
+import (
+	"github.com/v2fly/v2ray-core/v4/common/environment"
+	"github.com/v2fly/v2ray-core/v4/common/platform/filesystem"
+	"github.com/v2fly/v2ray-core/v4/common/platform/filesystem/fsifce"
+)
+
+type fileSystemDefaultImpl struct {
+}
+
+func (f fileSystemDefaultImpl) OpenFileForReadSeek() fsifce.FileSeekerFunc {
+	return filesystem.NewFileSeeker
+}
+
+func (f fileSystemDefaultImpl) OpenFileForRead() fsifce.FileReaderFunc {
+	return filesystem.NewFileReader
+}
+
+func (f fileSystemDefaultImpl) OpenFileForWrite() fsifce.FileWriterFunc {
+	return filesystem.NewFileWriter
+}
+
+func NewDefaultFileSystemDefaultImpl() environment.FileSystemCapabilitySet {
+	return fileSystemDefaultImpl{}
+}
