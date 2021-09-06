13f05c1b9d9771e8884cfbc23185aba1e7415576
Shelikhoo <xiaokangwang@outlook.com>
2021-09-06 13:42:05 +0100

    added service registration

---
+++ app/restful-api/config.go
@@ -0,0 +1,12 @@
+package restful_api
+
+import (
+	"context"
+	"github.com/v2fly/v2ray-core/v4/common"
+)
+
+func init() {
+	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
+		return newRestfulService(ctx, config.(*Config))
+	}))
+}
