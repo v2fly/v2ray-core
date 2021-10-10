d397e3864afd4830c0fd74f05da0e07bd13487e6
Vigilans <vigilans@foxmail.com>
2021-10-11 00:57:14 +0800

    Implement []matcherEntry as SimpleMatcherGroup

--- common/strmatcher/matchergroup_simple.go
+++ common/strmatcher/matchergroup_simple.go
@@ -1,10 +1,27 @@
 package strmatcher
 
 type matcherEntry struct {
-	m  Matcher
-	id uint32
+	matcher Matcher
+	value   uint32
 }
 
 type SimpleMatcherGroup struct {
 	matchers []matcherEntry
+}
+
+func (g *SimpleMatcherGroup) AddMatcher(matcher Matcher, value uint32) {
+	g.matchers = append(g.matchers, matcherEntry{
+		matcher: matcher,
+		value:   value,
+	})
+}
+
+func (g *SimpleMatcherGroup) Match(str string) []uint32 {
+	result := []uint32{}
+	for _, e := range g.matchers {
+		if e.matcher.Match(str) {
+			result = append(result, e.value)
+		}
+	}
+	return result
 }
