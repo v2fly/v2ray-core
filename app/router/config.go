b3b7907181586d6778d5bcd8b12674587557a4bf
Vigilans <vigilans@foxmail.com>
2021-10-14 22:50:29 +0800

    Compatibility fix for app/router's DomainMatcher condition

--- app/router/config.go
+++ app/router/config.go
@@ -39,23 +39,11 @@ func (rr *RoutingRule) BuildCondition() (Condition, error) {
 	conds := NewConditionChan()
 
 	if len(rr.Domain) > 0 {
-		switch rr.DomainMatcher {
-		case "mph", "hybrid":
-			matcher, err := NewMphMatcherGroup(rr.Domain)
-			if err != nil {
-				return nil, newError("failed to build domain condition with MphDomainMatcher").Base(err)
-			}
-			newError("MphDomainMatcher is enabled for ", len(rr.Domain), " domain rule(s)").AtDebug().WriteToLog()
-			conds.Add(matcher)
-		case "linear":
-			fallthrough
-		default:
-			matcher, err := NewDomainMatcher(rr.Domain)
-			if err != nil {
-				return nil, newError("failed to build domain condition").Base(err)
-			}
-			conds.Add(matcher)
+		cond, err := NewDomainMatcher(rr.DomainMatcher, rr.Domain)
+		if err != nil {
+			return nil, newError("failed to build domain condition").Base(err)
 		}
+		conds.Add(cond)
 	}
 
 	if len(rr.UserEmail) > 0 {
