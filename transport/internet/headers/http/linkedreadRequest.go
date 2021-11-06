94cee8b14e2069e8b61e5d60a77717b878790abe
Zhu Sheng Li <digglife@gmail.com>
2021-11-06 21:41:23 +0800

    fix: readRequest API changed since go1.17
    fixed: #1265

--- transport/internet/headers/http/linkedreadRequest.go
+++ transport/internet/headers/http/linkedreadRequest.go
@@ -9,4 +9,4 @@ import (
 )
 
 //go:linkname readRequest net/http.readRequest
-func readRequest(b *bufio.Reader, deleteHostHeader bool) (req *http.Request, err error)
+func readRequest(b *bufio.Reader) (req *http.Request, err error)
