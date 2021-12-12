fa61371102bc06425686de2817b8387d4dd916d7
世界 <i@sekai.icu>
2021-12-12 18:10:21 +0800

    Fix log usage

--- transport/internet/sockopt_linux.go
+++ transport/internet/sockopt_linux.go
@@ -61,13 +61,13 @@ func applyOutboundSocketOptions(network string, address string, fd uintptr, conf
 
 		if config.TcpKeepAliveInterval > 0 {
 			if err := syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, syscall.TCP_KEEPINTVL, int(config.TcpKeepAliveInterval)); err != nil {
-				return newError("failed to set TCP_KEEPINTVL", err)
+				return newError("failed to set TCP_KEEPINTVL").Base(err)
 			}
 			if err := syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, syscall.TCP_KEEPIDLE, int(config.TcpKeepAliveIdle)); err != nil {
-				return newError("failed to set TCP_KEEPIDLE", err)
+				return newError("failed to set TCP_KEEPIDLE").Base(err)
 			}
 			if err := syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_KEEPALIVE, 1); err != nil {
-				return newError("failed to set SO_KEEPALIVE", err)
+				return newError("failed to set SO_KEEPALIVE").Base(err)
 			}
 		}
 	}
