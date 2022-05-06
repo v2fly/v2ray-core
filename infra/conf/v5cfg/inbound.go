4ea9a5638e48c70830f32ebded657d78e663c596
Huang-Huang Bao <eh5@sokka.cn>
2022-05-07 01:41:27 +0800

    Fix: correct a type assertion of dokodemo simplified config
    Fixes tproxy not working with jsonv5 config

--- infra/conf/v5cfg/inbound.go
+++ infra/conf/v5cfg/inbound.go
@@ -69,7 +69,7 @@ func (c InboundConfig) BuildV5(ctx context.Context) (proto.Message, error) {
 		return nil, newError("unable to load inbound protocol config").Base(err)
 	}
 
-	if content, ok := inboundConfigPack.(*dokodemo.Config); ok {
+	if content, ok := inboundConfigPack.(*dokodemo.SimplifiedConfig); ok {
 		receiverSettings.ReceiveOriginalDestination = content.FollowRedirect
 	}
 
