4bdb4117479c174974daf3502396c6fdde2d4098
秋のかえで <autmaple@protonmail.com>
2022-09-07 21:42:25 +0800

    Style: remove duplicate FetchHTTPContent function

--- common/cmdarg/arg.go
+++ common/cmdarg/arg.go
@@ -4,12 +4,6 @@ import (
 	"bytes"
 	"io"
 	"io/ioutil"
-	"net/http"
-	"net/url"
-	"strings"
-	"time"
-
-	"github.com/v2fly/v2ray-core/v5/common/buf"
 )
 
 // LoadArg loads one arg, maybe an remote url, or local file path
@@ -29,40 +23,4 @@ func LoadArgToBytes(arg string) (out []byte, err error) {
 		return
 	}
 	return
-}
-
-// FetchHTTPContent dials https for remote content
-func FetchHTTPContent(target string) ([]byte, error) {
-	parsedTarget, err := url.Parse(target)
-	if err != nil {
-		return nil, newError("invalid URL: ", target).Base(err)
-	}
-
-	if s := strings.ToLower(parsedTarget.Scheme); s != "http" && s != "https" {
-		return nil, newError("invalid scheme: ", parsedTarget.Scheme)
-	}
-
-	client := &http.Client{
-		Timeout: 30 * time.Second,
-	}
-	resp, err := client.Do(&http.Request{
-		Method: "GET",
-		URL:    parsedTarget,
-		Close:  true,
-	})
-	if err != nil {
-		return nil, newError("failed to dial to ", target).Base(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return nil, newError("unexpected HTTP status code: ", resp.StatusCode)
-	}
-
-	content, err := buf.ReadAllToBytes(resp.Body)
-	if err != nil {
-		return nil, newError("failed to read HTTP response").Base(err)
-	}
-
-	return content, nil
 }
