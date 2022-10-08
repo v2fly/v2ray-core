5593fdee092de3c1c8afe53ee8ae17e8c54a8641
秋のかえで <autmaple@protonmail.com>
2022-10-08 09:03:51 +0800

    Feat: add bind to device to Windows and Darwin (#1972)

--- transport/internet/sockopt_windows.go
+++ transport/internet/sockopt_windows.go
@@ -1,13 +1,16 @@
 package internet
 
 import (
+	"net"
 	"syscall"
 
 	"golang.org/x/sys/windows"
 )
 
 const (
-	TCP_FASTOPEN = 15 // nolint: revive,stylecheck
+	TCP_FASTOPEN    = 15 // nolint: revive,stylecheck
+	IP_UNICAST_IF   = 31 // nolint: revive,stylecheck
+	IPV6_UNICAST_IF = 31 // nolint: revive,stylecheck
 )
 
 func setTFO(fd syscall.Handle, settings SocketConfig_TCPFastOpenState) error {
@@ -33,6 +36,19 @@ func applyOutboundSocketOptions(network string, address string, fd uintptr, conf
 			if err := syscall.SetsockoptInt(syscall.Handle(fd), syscall.SOL_SOCKET, syscall.SO_KEEPALIVE, 1); err != nil {
 				return newError("failed to set SO_KEEPALIVE", err)
 			}
+		}
+	}
+
+	if config.BindToDevice != "" {
+		iface, err := net.InterfaceByName(config.BindToDevice)
+		if err != nil {
+			return newError("failed to get interface ", config.BindToDevice).Base(err)
+		}
+		if err := windows.SetsockoptInt(windows.Handle(fd), windows.IPPROTO_IP, IP_UNICAST_IF, iface.Index); err != nil {
+			return newError("failed to set IP_UNICAST_IF", err)
+		}
+		if err := windows.SetsockoptInt(windows.Handle(fd), windows.IPPROTO_IPV6, IPV6_UNICAST_IF, iface.Index); err != nil {
+			return newError("failed to set IPV6_UNICAST_IF", err)
 		}
 	}
 
