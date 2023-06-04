620c2c1e3776cb9c699f3c9880bb2634bfb98f33
57a4c324e742 <55197263+57a4c324e742@users.noreply.github.com>
2023-06-04 01:46:12 +0000

    Add DomainStrategy for JSONv5 outbound

--- infra/conf/v5cfg/skeleton.go
+++ infra/conf/v5cfg/skeleton.go
@@ -31,13 +31,14 @@ type InboundConfig struct {
 }
 
 type OutboundConfig struct {
-	Protocol      string                `json:"protocol"`
-	SendThrough   *cfgcommon.Address    `json:"sendThrough"`
-	Tag           string                `json:"tag"`
-	Settings      json.RawMessage       `json:"settings"`
-	StreamSetting *StreamConfig         `json:"streamSettings"`
-	ProxySettings *proxycfg.ProxyConfig `json:"proxySettings"`
-	MuxSettings   *muxcfg.MuxConfig     `json:"mux"`
+	Protocol       string                `json:"protocol"`
+	SendThrough    *cfgcommon.Address    `json:"sendThrough"`
+	Tag            string                `json:"tag"`
+	Settings       json.RawMessage       `json:"settings"`
+	StreamSetting  *StreamConfig         `json:"streamSettings"`
+	ProxySettings  *proxycfg.ProxyConfig `json:"proxySettings"`
+	MuxSettings    *muxcfg.MuxConfig     `json:"mux"`
+	DomainStrategy string                `json:"domainStrategy"`
 }
 
 type StreamConfig struct {
