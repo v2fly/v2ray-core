bda47527de82a3909ab7d002eb631c8df402d7a8
Kamandlou <ahkamandlou@gmail.com>
2023-08-04 11:09:55 +0330

    fix: general.go

--- infra/conf/cfgcommon/testassist/general.go
+++ infra/conf/cfgcommon/testassist/general.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"google.golang.org/protobuf/proto"
+	"github.com/golang/protobuf/proto"
 
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/infra/conf/cfgcommon"
