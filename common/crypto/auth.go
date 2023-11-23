268306702fea22785febdb6c6ac745ea9e57cdce
Shelikhoo <xiaokangwang@outlook.com>
2023-11-23 23:59:53 +0000

    Shadowsocks2022 Client Implementation Improvements (again)
    1. Fixed packet size uint16 overflow issue

--- common/crypto/auth.go
+++ common/crypto/auth.go
@@ -91,6 +91,7 @@ type AuthenticationReader struct {
 	transferType protocol.TransferType
 	padding      PaddingLengthGenerator
 	size         uint16
+	sizeOffset   uint16
 	paddingLen   uint16
 	hasSize      bool
 	done         bool
@@ -103,6 +104,9 @@ func NewAuthenticationReader(auth Authenticator, sizeParser ChunkSizeDecoder, re
 		transferType: transferType,
 		padding:      paddingLen,
 		sizeBytes:    make([]byte, sizeParser.SizeBytes()),
+	}
+	if chunkSizeDecoderWithOffset, ok := sizeParser.(ChunkSizeDecoderWithOffset); ok {
+		r.sizeOffset = chunkSizeDecoderWithOffset.HasConstantOffset()
 	}
 	if breader, ok := reader.(*buf.BufferedReader); ok {
 		r.reader = breader
@@ -160,12 +164,14 @@ func (r *AuthenticationReader) readInternal(soft bool, mb *buf.MultiBuffer) erro
 		return err
 	}
 
-	if size == uint16(r.auth.Overhead())+padding {
+	if size+r.sizeOffset == uint16(r.auth.Overhead())+padding {
 		r.done = true
 		return io.EOF
 	}
 
-	if soft && int32(size) > r.reader.BufferedBytes() {
+	effectiveSize := int32(size) + int32(r.sizeOffset)
+
+	if soft && effectiveSize > r.reader.BufferedBytes() {
 		r.size = size
 		r.paddingLen = padding
 		r.hasSize = true
@@ -173,7 +179,7 @@ func (r *AuthenticationReader) readInternal(soft bool, mb *buf.MultiBuffer) erro
 	}
 
 	if size <= buf.Size {
-		b, err := r.readBuffer(int32(size), int32(padding))
+		b, err := r.readBuffer(effectiveSize, int32(padding))
 		if err != nil {
 			return nil
 		}
@@ -181,16 +187,16 @@ func (r *AuthenticationReader) readInternal(soft bool, mb *buf.MultiBuffer) erro
 		return nil
 	}
 
-	payload := bytespool.Alloc(int32(size))
+	payload := bytespool.Alloc(effectiveSize)
 	defer bytespool.Free(payload)
 
-	if _, err := io.ReadFull(r.reader, payload[:size]); err != nil {
+	if _, err := io.ReadFull(r.reader, payload[:effectiveSize]); err != nil {
 		return err
 	}
 
-	size -= padding
+	effectiveSize -= int32(padding)
 
-	rb, err := r.auth.Open(payload[:0], payload[:size])
+	rb, err := r.auth.Open(payload[:0], payload[:effectiveSize])
 	if err != nil {
 		return err
 	}
--- proxy/shadowsocks2022/encoding.go
+++ proxy/shadowsocks2022/encoding.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"crypto/cipher"
 	cryptoRand "crypto/rand"
+	"encoding/binary"
+	"github.com/v2fly/v2ray-core/v5/common"
 	"io"
 	"math/rand"
 	"time"
@@ -243,7 +245,7 @@ func (t *TCPRequest) CreateClientS2CReader(in io.Reader, initialPayload *buf.Buf
 	if err != nil {
 		return nil, newError("failed to decrypt initial payload").Base(err)
 	}
-	return crypto.NewAuthenticationReader(AEADAuthenticator, &crypto.AEADChunkSizeParser{
+	return crypto.NewAuthenticationReader(AEADAuthenticator, &AEADChunkSizeParser{
 		Auth: AEADAuthenticator,
 	}, in, protocol.TransferTypeStream, nil), nil
 }
@@ -258,4 +260,31 @@ func (t *TCPRequest) CreateClientC2SWriter(writer io.Writer) buf.Writer {
 		Auth: AEADAuthenticator,
 	}
 	return crypto.NewAuthenticationWriter(AEADAuthenticator, sizeParser, writer, protocol.TransferTypeStream, nil)
+}
+
+type AEADChunkSizeParser struct {
+	Auth *crypto.AEADAuthenticator
+}
+
+func (p *AEADChunkSizeParser) HasConstantOffset() uint16 {
+	return uint16(p.Auth.Overhead())
+}
+
+func (p *AEADChunkSizeParser) SizeBytes() int32 {
+	return 2 + int32(p.Auth.Overhead())
+}
+
+func (p *AEADChunkSizeParser) Encode(size uint16, b []byte) []byte {
+	binary.BigEndian.PutUint16(b, size-uint16(p.Auth.Overhead()))
+	b, err := p.Auth.Seal(b[:0], b[:2])
+	common.Must(err)
+	return b
+}
+
+func (p *AEADChunkSizeParser) Decode(b []byte) (uint16, error) {
+	b, err := p.Auth.Open(b[:0], b)
+	if err != nil {
+		return 0, err
+	}
+	return binary.BigEndian.Uint16(b), nil
 }
