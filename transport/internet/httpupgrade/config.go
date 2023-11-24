57d712a4d478aaba0821b1da8bdbf0a4c5fd9efc
ghosx <ghosx@qq.com>
2023-11-24 08:51:49 +0800

    fix: unexpected path in httpugprade (#2773)

---
+++ transport/internet/httpupgrade/config.go
@@ -0,0 +1,12 @@
+package httpupgrade
+
+func (c *Config) GetNormalizedPath() string {
+	path := c.Path
+	if path == "" {
+		return "/"
+	}
+	if path[0] != '/' {
+		return "/" + path
+	}
+	return path
+}
