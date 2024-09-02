3a88a4bbda5ca15954af42f0483ae15fb4ce248e
Misaki Kasumi <misakikasumi@outlook.com>
2024-09-03 01:07:42 +0800

    add more checks

--- transport/internet/socket_activation_unix.go
+++ transport/internet/socket_activation_unix.go
@@ -4,19 +4,45 @@
 package internet
 
 import (
+	"fmt"
 	"os"
+	"path"
 	"strconv"
 	"syscall"
 
 	"github.com/v2fly/v2ray-core/v5/common/net"
 )
 
-func activate_socket(address string) (net.Listener, error) {
-	fd, err := strconv.Atoi(address[8:])
+func activateSocket(address string) (net.Listener, error) {
+	fd, err := strconv.Atoi(path.Base(address))
 	if err != nil {
 		return nil, err
 	}
-	// Ignore the fail of SetNonblock: it's merely an optimization so that Go can poll this fd.
-	_ = syscall.SetNonblock(fd, true)
-	return net.FileListener(os.NewFile(uintptr(fd), address))
+
+	err = syscall.SetNonblock(fd, true)
+	if err != nil {
+		return nil, err
+	}
+
+	acceptConn, err := syscall.GetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_ACCEPTCONN)
+	if err != nil {
+		return nil, err
+	}
+	if acceptConn == 0 {
+		return nil, fmt.Errorf("socket '%s' has not been marked to accept connections", address)
+	}
+
+	sockType, err := syscall.GetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_TYPE)
+	if err != nil {
+		return nil, err
+	}
+	if sockType != syscall.SOCK_STREAM {
+		// XXX: currently only stream socks are supported
+		return nil, fmt.Errorf("socket '%s' is not a stream socket", address)
+	}
+
+	file := os.NewFile(uintptr(fd), address)
+	defer file.Close()
+
+	return net.FileListener(file)
 }
