7ae33b58620d35ac9628eb342ceec65f2a8635f3
Shelikhoo <xiaokangwang@outlook.com>
2025-05-18 22:04:35 +0100

    add client initial implementation for tls mirror

---
+++ transport/internet/tlsmirror/server/client.go
@@ -0,0 +1,108 @@
+package server
+
+import (
+	"context"
+
+	"github.com/v2fly/v2ray-core/v5/common"
+	"github.com/v2fly/v2ray-core/v5/common/environment"
+	"github.com/v2fly/v2ray-core/v5/common/environment/envctx"
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/mirrorbase"
+)
+
+func newPersistentMirrorTLSDialer() *persistentMirrorTLSDialer {
+	return &persistentMirrorTLSDialer{}
+
+}
+
+type persistentMirrorTLSDialer struct {
+	ctx context.Context
+
+	config *Config
+
+	requestNewConnection func(ctx context.Context) error
+	incomingConnections  chan net.Conn
+}
+
+func (d *persistentMirrorTLSDialer) handleIncomingCarrierConnection(ctx context.Context, conn net.Conn) {
+	transportEnvironment := envctx.EnvironmentFromContext(d.ctx).(environment.TransportEnvironment)
+	dialer := transportEnvironment.OutboundDialer()
+
+	port, err := net.PortFromInt(d.config.ForwardPort)
+	if err != nil {
+		newError("failed to parse port").Base(err).AtWarning().WriteToLog()
+		return
+	}
+
+	address := net.ParseAddress(d.config.ForwardAddress)
+
+	dest := net.TCPDestination(address, port)
+
+	forwardConn, err := dialer(d.ctx, dest, d.config.ForwardTag)
+	if err != nil {
+		newError("failed to dial to destination").Base(err).AtWarning().WriteToLog()
+		return
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	cconnState := &clientConnState{
+		ctx:        ctx,
+		done:       cancel,
+		localAddr:  conn.LocalAddr(),
+		remoteAddr: conn.RemoteAddr(),
+		handler:    d.handleIncomingReadyConnection,
+	}
+
+	cconnState.mirrorConn = mirrorbase.NewMirroredTLSConn(ctx, conn, forwardConn, cconnState.onC2SMessage, cconnState.onS2CMessage, conn)
+}
+
+func (d *persistentMirrorTLSDialer) handleIncomingReadyConnection(conn internet.Connection) {
+	d.incomingConnections <- conn
+}
+
+func (d *persistentMirrorTLSDialer) Dial(ctx context.Context,
+	dest net.Destination, settings *internet.MemoryStreamConfig) (internet.Connection, error) {
+	var recvConn net.Conn
+	select {
+	case conn := <-d.incomingConnections:
+		recvConn = conn
+	default:
+		err := d.requestNewConnection(ctx)
+		if err != nil {
+			return nil, newError("failed to request new connection").Base(err)
+		}
+		select {
+		case conn := <-d.incomingConnections:
+			recvConn = conn
+		}
+	}
+
+	if recvConn == nil {
+		return nil, newError("failed to receive connection")
+	}
+
+	return recvConn, nil
+
+}
+
+func Dial(ctx context.Context, dest net.Destination, settings *internet.MemoryStreamConfig) (internet.Connection, error) {
+	transportEnvironment := envctx.EnvironmentFromContext(ctx).(environment.TransportEnvironment)
+	dialer, err := transportEnvironment.TransientStorage().Get(ctx, "persistentDialer")
+	if err != nil {
+		dialer = newPersistentMirrorTLSDialer()
+		err = transportEnvironment.TransientStorage().Put(ctx, "persistentDialer", dialer)
+		if err != nil {
+			return nil, newError("failed to put persistent dialer").Base(err)
+		}
+	}
+	conn, err := dialer.(*persistentMirrorTLSDialer).Dial(ctx, dest, settings)
+	if err != nil {
+		return nil, newError("failed to dial").Base(err)
+	}
+	return conn, nil
+}
+
+func init() {
+	common.Must(internet.RegisterTransportDialer(protocolName, Dial))
+}
---
+++ transport/internet/tlsmirror/server/client_conn.go
@@ -0,0 +1,159 @@
+package server
+
+import (
+	"bytes"
+	"context"
+	gonet "net"
+	"time"
+
+	"github.com/v2fly/v2ray-core/v5/common/net"
+	"github.com/v2fly/v2ray-core/v5/transport/internet"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/mirrorcommon"
+	"github.com/v2fly/v2ray-core/v5/transport/internet/tlsmirror/mirrorcrypto"
+)
+
+type clientConnState struct {
+	ctx     context.Context
+	done    context.CancelFunc
+	handler internet.ConnHandler
+
+	mirrorConn tlsmirror.InsertableTLSConn
+	localAddr  net.Addr
+	remoteAddr net.Addr
+
+	activated bool
+	decryptor *mirrorcrypto.Decryptor
+	encryptor *mirrorcrypto.Encryptor
+
+	primaryKey []byte
+
+	readPipe   chan []byte
+	readBuffer *bytes.Buffer
+
+	protocolVersion [2]byte
+}
+
+func (s *clientConnState) Read(b []byte) (n int, err error) {
+	if s.readBuffer != nil {
+		n, _ = s.readBuffer.Read(b)
+		if n > 0 {
+			return n, nil
+		}
+		s.readBuffer = nil
+	}
+
+	select {
+	case <-s.ctx.Done():
+		return 0, s.ctx.Err()
+	case data := <-s.readPipe:
+		s.readBuffer = bytes.NewBuffer(data)
+		n, err = s.readBuffer.Read(b)
+		if err != nil {
+			return 0, err
+		}
+		return n, nil
+	}
+}
+
+func (s *clientConnState) Write(b []byte) (n int, err error) {
+	err = s.WriteMessage(b)
+	if err != nil {
+		return 0, err
+	}
+	n = len(b)
+	return n, nil
+}
+
+func (s *clientConnState) Close() error {
+	s.done()
+	return nil
+}
+
+func (s *clientConnState) LocalAddr() gonet.Addr {
+	return s.remoteAddr
+}
+
+func (s *clientConnState) RemoteAddr() gonet.Addr {
+	return s.remoteAddr
+}
+
+func (s *clientConnState) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (s *clientConnState) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (s *clientConnState) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+
+func (s *clientConnState) onC2SMessage(message *tlsmirror.TLSRecord) (drop bool, ok error) {
+	if message.RecordType == mirrorcommon.TLSRecord_RecordType_application_data {
+		if s.decryptor == nil {
+			clientRandom, serverRandom, err := s.mirrorConn.GetHandshakeRandom()
+			if err != nil {
+				return false, nil
+			}
+
+			{
+				encryptionKey, nonceMask, err := mirrorcrypto.DeriveEncryptionKey(s.primaryKey, clientRandom, serverRandom, ":s2c")
+				if err != nil {
+					newError("failed to derive C2S encryption key").Base(err).AtWarning().WriteToLog()
+					return false, nil
+				}
+				s.decryptor = mirrorcrypto.NewDecryptor(encryptionKey, nonceMask)
+			}
+
+			{
+				encryptionKey, nonceMask, err := mirrorcrypto.DeriveEncryptionKey(s.primaryKey, clientRandom, serverRandom, ":c2s")
+				if err != nil {
+					newError("failed to derive S2C encryption key").Base(err).AtWarning().WriteToLog()
+					return false, nil
+				}
+				s.encryptor = mirrorcrypto.NewEncryptor(encryptionKey, nonceMask)
+			}
+			s.protocolVersion = message.LegacyProtocolVersion
+
+			if !s.activated {
+				s.handler(s)
+				s.activated = true
+			}
+		}
+	}
+	return false, ok
+}
+
+func (s *clientConnState) onS2CMessage(message *tlsmirror.TLSRecord) (drop bool, ok error) {
+	if message.RecordType == mirrorcommon.TLSRecord_RecordType_application_data {
+		if s.encryptor == nil {
+			return false, nil
+		}
+		buffer := make([]byte, 0, len(message.Fragment)-s.encryptor.NonceSize())
+		buffer, err := s.decryptor.Open(buffer, message.Fragment)
+		if err != nil {
+			return false, nil
+		}
+
+		s.readPipe <- buffer
+		return true, nil
+	}
+	return false, ok
+}
+
+func (s *clientConnState) WriteMessage(message []byte) error {
+	buffer := make([]byte, 0, len(message)+s.encryptor.NonceSize())
+	buffer, err := s.encryptor.Seal(buffer, message)
+	if err != nil {
+		return newError("failed to encrypt message").Base(err)
+	}
+	record := tlsmirror.TLSRecord{
+		RecordType:            mirrorcommon.TLSRecord_RecordType_application_data,
+		LegacyProtocolVersion: s.protocolVersion,
+		RecordLength:          uint16(len(buffer)),
+		Fragment:              buffer,
+	}
+	return s.mirrorConn.InsertC2SMessage(&record)
+}
