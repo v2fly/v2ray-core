c2aa4c9f5449ecccb3e4f51747bc89bf00c63d94
Pk-web6936 <202365630+Pk-web6936@users.noreply.github.com>
2025-05-18 08:09:28 +0330

    Update device.go

--- app/tun/device/device.go
+++ app/tun/device/device.go
@@ -2,15 +2,15 @@ package device
 
 import (
 	"gvisor.dev/gvisor/pkg/tcpip/stack"
+	"github.com/v2fly/v2ray-core/v5/common"
 )
 
 //go:generate go run github.com/v2fly/v2ray-core/v5/common/errors/errorgen
 
 type Device interface {
 	stack.LinkEndpoint
-	// The Close() method is already included if stack.LinkEndpoint defines it.
-	// If you need an explicit Close() method with a specific signature, you can uncomment the following line:
-	// Close() error
+	
+	common.Closable
 }
 
 type Options struct {
