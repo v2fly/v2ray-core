7c5e4c7a4b46c3cc446aef387d16845be8ddc731
yiitz <fufulove2012@gmail.com>
2025-05-27 06:15:48 +0800

    Fix mkcp unreleased buffer (#3411)
    * Fix buffer leak in mkcp transport protocol
    
    * fix Write panic caused by concurrent state check

--- transport/internet/kcp/output.go
+++ transport/internet/kcp/output.go
@@ -10,12 +10,14 @@ import (
 
 type SegmentWriter interface {
 	Write(seg Segment) error
+	Release()
 }
 
 type SimpleSegmentWriter struct {
 	sync.Mutex
 	buffer *buf.Buffer
 	writer io.Writer
+	closed bool
 }
 
 func NewSegmentWriter(writer io.Writer) SegmentWriter {
@@ -28,12 +30,22 @@ func NewSegmentWriter(writer io.Writer) SegmentWriter {
 func (w *SimpleSegmentWriter) Write(seg Segment) error {
 	w.Lock()
 	defer w.Unlock()
+	if w.closed {
+		return io.ErrClosedPipe
+	}
 
 	w.buffer.Clear()
 	rawBytes := w.buffer.Extend(seg.ByteSize())
 	seg.Serialize(rawBytes)
 	_, err := w.writer.Write(w.buffer.Bytes())
 	return err
+}
+
+func (w *SimpleSegmentWriter) Release() {
+	w.Lock()
+	defer w.Unlock()
+	w.buffer.Release()
+	w.closed = true
 }
 
 type RetryableWriter struct {
@@ -50,4 +62,8 @@ func (w *RetryableWriter) Write(seg Segment) error {
 	return retry.Timed(5, 100).On(func() error {
 		return w.writer.Write(seg)
 	})
+}
+
+func (w *RetryableWriter) Release() {
+	w.writer.Release()
 }
