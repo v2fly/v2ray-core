f0c0614724f4c34983162e181963ee4bbbb1608a
Shelikhoo <xiaokangwang@outlook.com>
2025-07-03 23:40:39 +0100

    update nonce counter for tls12 encrypted alert as well.

--- transport/internet/tlsmirror/mirrorcommon/record_consts.go
+++ transport/internet/tlsmirror/mirrorcommon/record_consts.go
@@ -4,4 +4,5 @@ const (
 	TLSRecord_RecordType_application_data   = 23
 	TLSRecord_RecordType_handshake          = 22
 	TLSRecord_RecordType_change_cipher_spec = 20
+	TLSRecord_RecordType_alert              = 21
 )
