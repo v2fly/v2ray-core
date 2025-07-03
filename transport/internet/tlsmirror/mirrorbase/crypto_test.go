c5199a2f6fc23dc27f48084a1c1a04f3d55e6682
Shelikhoo <xiaokangwang@outlook.com>
2025-07-03 23:30:11 +0100

    fix corrupted tls12 aead stream in tls mirror

---
+++ transport/internet/tlsmirror/mirrorbase/crypto_test.go
@@ -0,0 +1,27 @@
+package mirrorbase
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"testing"
+
+	"github.com/v2fly/v2ray-core/v5/common/crypto"
+)
+
+func TestTLS12ExplicitNonceGeneration(t *testing.T) {
+	generator := reverseBytesGeneratorByteOrder(crypto.GenerateIncreasingNonce([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}))
+
+	firstValue := generator()
+	if diff := cmp.Diff(firstValue, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}); diff != "" {
+		t.Errorf("Unexpected first value: %s", diff)
+	}
+
+	secondValue := generator()
+	if diff := cmp.Diff(secondValue, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02}); diff != "" {
+		t.Errorf("Unexpected second value: %s", diff)
+	}
+
+	thirdValue := generator()
+	if diff := cmp.Diff(thirdValue, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03}); diff != "" {
+		t.Errorf("Unexpected third value: %s", diff)
+	}
+}
