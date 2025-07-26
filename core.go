e880c61f86dd9098299ccf5ab6709a1896488226
Shelikhoo <xiaokangwang@outlook.com>
2025-07-26 15:14:17 +0100

    update version to v5.38.0

--- core.go
+++ core.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	version  = "5.37.0"
+	version  = "5.38.0"
 	build    = "Custom"
 	codename = "V2Fly, a community-driven edition of V2Ray."
 	intro    = "A unified platform for anti-censorship."
