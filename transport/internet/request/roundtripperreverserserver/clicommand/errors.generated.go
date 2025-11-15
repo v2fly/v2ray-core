dcd9bd987ca8b97137cd59be0bdede59964aa212
Shelikhoo <xiaokangwang@outlook.com>
2025-11-15 16:49:53 +0000

    Add request rtt reverser tool

---
+++ transport/internet/request/roundtripperreverserserver/clicommand/errors.generated.go
@@ -0,0 +1,9 @@
+package clicommand
+
+import "github.com/v2fly/v2ray-core/v5/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}
