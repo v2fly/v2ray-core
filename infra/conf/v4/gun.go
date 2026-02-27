29cdb33b85020a7119c2de5950195c0c5086453e
Leo <i@hardrain980.com>
2026-02-27 22:00:40 +0800

    feat: grpc: allow configurable keepalive and initial windows size
    added configuration options:
    
    	- idle_timeout
    	- health_check_timeout
    	- permit_without_stream
    	- initial_windows_size
    
    acknowledge:
    
    	- https://github.com/XTLS/Xray-core
    	- https://xtls.github.io/en/config/transports/grpc.html
    
    Signed-off-by: Leo <i@hardrain980.com>

--- infra/conf/v4/gun.go
+++ infra/conf/v4/gun.go
@@ -7,9 +7,28 @@ import (
 )
 
 type GunConfig struct {
-	ServiceName string `json:"serviceName"`
+	ServiceName         string `json:"serviceName"`
+	IdleTimeout         int32  `json:"idle_timeout"`
+	HealthCheckTimeout  int32  `json:"health_check_timeout"`
+	PermitWithoutStream bool   `json:"permit_without_stream"`
+	InitialWindowsSize  int32  `json:"initial_windows_size"`
 }
 
 func (g GunConfig) Build() (proto.Message, error) {
-	return &grpc.Config{ServiceName: g.ServiceName}, nil
+	if g.IdleTimeout <= 0 {
+		g.IdleTimeout = 0
+	}
+	if g.HealthCheckTimeout <= 0 {
+		g.HealthCheckTimeout = 0
+	}
+	if g.InitialWindowsSize < 0 {
+		g.InitialWindowsSize = 0
+	}
+	return &grpc.Config{
+		ServiceName:         g.ServiceName,
+		IdleTimeout:         g.IdleTimeout,
+		HealthCheckTimeout:  g.HealthCheckTimeout,
+		PermitWithoutStream: g.PermitWithoutStream,
+		InitialWindowsSize:  g.InitialWindowsSize,
+	}, nil
 }
